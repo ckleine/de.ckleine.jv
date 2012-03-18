011560ba9608e2a9ab9a08a793207f3aa1de3e8b
Christoph Kleine <c.kleine10@googlemail.com>
2012-03-18 18:13:37 +0100

    new set lenient

--- JVerleihNix/src/jverleihnix/model/Validation.java
+++ JVerleihNix/src/jverleihnix/model/Validation.java
@@ -2,11 +2,7 @@ package jverleihnix.model;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.GregorianCalendar;
-import java.lang.Integer;
-
 import jverleihnix.ui.MediaType;
 
 public class Validation {
@@ -29,128 +25,14 @@ public class Validation {
 		if (date == null) {
 			return false;
 		}
-		String dateFormatString = getDateFormat(dueDateString);
-
 		Date actuallDate = new Date();
-
-		final int MAX_DAY_OF_MONTH = getMaxDayOfMonth(
-				getMonth(dateFormatString, dueDateString),
-				getYear(dateFormatString, dueDateString));
 
 		// check future date
 		if (date.before(actuallDate)) {
 			return false;
-
-			// check month
-		} else if (getMonth(dateFormatString, dueDateString) > 12
-				|| getMonth(dateFormatString, dueDateString) < 1) {
-			return false;
-			// check day
-		} else if (getDay(dateFormatString, dueDateString) > MAX_DAY_OF_MONTH
-				|| getDay(dateFormatString, dueDateString) < 1) {
-			return false;
-
-			// check time for germanDateFormat
-		} else if (dateFormatString.contains("h")) {
-			if (getHour(dateFormatString, dueDateString) > 23
-					|| getDay(dateFormatString, dueDateString) < 0) {
-				return false;
-			} else if (getMin(dateFormatString, dueDateString) > 59
-					|| getDay(dateFormatString, dueDateString) < 0) {
-				return false;
-			}
-			// check time for englishDateFormat
-		} else if (dateFormatString.contains("a")) {
-			if (getHour(dateFormatString, dueDateString) > 12
-					|| getDay(dateFormatString, dueDateString) < 1) {
-				return false;
-			} else if (getMin(dateFormatString, dueDateString) > 59
-					|| getDay(dateFormatString, dueDateString) < 0) {
-				return false;
-			}
 		}
-
 		return true;
 
-	}
-	/**
-	 * @param dateFormatString Format of the date string
-	 * @param dueDate Date String
-	 * @return Month of the date
-	 * @throws NumberFormatException if substring (month) parse to integer fails 
-	 */
-	private static int getMonth(String dateFormatString, String dueDate)
-			throws NumberFormatException {
-		String month = "" + dueDate.charAt(dateFormatString.indexOf('M'))
-				+ dueDate.charAt(dateFormatString.indexOf('M') + 1);
-		return Integer.parseInt(month);
-	}
-	
-	/**
-	 * @param dateFormatString Format of the date string
-	 * @param dueDate Date String
-	 * @return Day of the date
-	 * @throws NumberFormatException if substring (day) parse to integer fails 
-	 */
-	private static int getDay(String dateFormatString, String dueDate)
-			throws NumberFormatException {
-		String day = "" + dueDate.charAt(dateFormatString.indexOf('d'))
-				+ dueDate.charAt(dateFormatString.indexOf('d') + 1);
-		return Integer.parseInt(day);
-	}
-	
-	/**
-	 * @param dateFormatString Format of the date string
-	 * @param dueDate Date String
-	 * @return Year of the date
-	 * @throws NumberFormatException if substring (year) parse to integer fails 
-	 */
-	private static int getYear(String dateFormatString, String dueDate)
-			throws NumberFormatException {
-		String year = "" + dueDate.charAt(dateFormatString.indexOf('y'))
-				+ dueDate.charAt(dateFormatString.indexOf('y') + 1)
-				+ dueDate.charAt(dateFormatString.indexOf('y') + 2)
-				+ dueDate.charAt(dateFormatString.indexOf('y') + 3);
-		return Integer.parseInt(year);
-	}
-	
-	/**
-	 * @param dateFormatString Format of the date string
-	 * @param dueDate Date String
-	 * @return Minutes of the date
-	 * @throws NumberFormatException if substring (minutes) parse to integer fails 
-	 */
-	private static int getMin(String dateFormatString, String dueDate)
-			throws NumberFormatException {
-		String min = "" + dueDate.charAt(dateFormatString.indexOf('m'))
-				+ dueDate.charAt(dateFormatString.indexOf('m') + 1);
-		return Integer.parseInt(min);
-	}
-	
-	/**
-	 * @param dateFormatString Format of the date string
-	 * @param dueDate Date String
-	 * @return Hour of the date
-	 * @throws NumberFormatException if substring (hour) parse to integer fails 
-	 */
-	private static int getHour(String dateFormatString, String dueDate)
-			throws NumberFormatException {
-		String hour = "" + dueDate.charAt(dateFormatString.indexOf('h'))
-				+ dueDate.charAt(dateFormatString.indexOf('h') + 1);
-		return Integer.parseInt(hour);
-	}
-	
-	/**
-	 * @param month of the date
-	 * @param year of the date
-	 * @return the last day of the specific month
-	 */
-	private static int getMaxDayOfMonth(int month, int year) {
-		Calendar cal = new GregorianCalendar();
-		cal.set(Calendar.YEAR, year);
-		cal.set(Calendar.MONTH, month - 1);
-		cal.set(Calendar.DATE, 1);
-		return cal.getActualMaximum(Calendar.DATE);
 	}
 
 	/**
@@ -168,32 +50,22 @@ public class Validation {
 						dateFormatString);
 				dateFormat.setLenient(false);
 				try {
-					/* Catch wrong dateStrings like 11.1111.11 (dd.MM.yyyy)
-					getYear(dateFormatString, dateString);
-					getMonth(dateFormatString, dateString);
-					getDay(dateFormatString, dateString);
-					if (dateFormatString.contains("h")) {
-						getHour(dateFormatString, dateString);
-						getMin(dateFormatString, dateString);
-					}*/
-
 					date = dateFormat.parse(dateString);
 				} catch (ParseException exc) {
 					return date;
-				} catch (NumberFormatException exc) {
-					return date;
 				}
-
 			}
 		}
 		return date;
 	}
-	
+
 	/**
-	 *  Gives the date Format of an date String
+	 * Gives the date Format of an date String
 	 * 
-	 * @param dateString Date String to get the date format
-	 * @return the date Format of the date String or null if there is no correct date Format
+	 * @param dateString
+	 *            Date String to get the date format
+	 * @return the date Format of the date String or null if there is no correct
+	 *         date Format
 	 */
 	public static String getDateFormat(String dateString) {
 		String dateFormat = null;
@@ -205,9 +77,9 @@ public class Validation {
 		return dateFormat;
 	}
 
-	
 	private static boolean valDate(String dateString, String dateFormatString) {
 		SimpleDateFormat dateFormat = new SimpleDateFormat(dateFormatString);
+		dateFormat.setLenient(false);
 		try {
 			int length = dateFormatString.length();
 			if (dateFormatString.contains("a")) {
